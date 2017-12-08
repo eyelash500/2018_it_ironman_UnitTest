cc7b2aca23acaeaae9995fc82c279664e5e455f2
eyelash500 <eyelash94500@gmail.com>
2017-12-09 01:13:40 +0800

    init and the first Unit Test.

---
+++ LearningUT/LogAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LearningUT
+{
+    class LogAnalyzer
+    {
+        public static void Main()
+        {
+
+        }
+        public bool IsValidLogFileName(string fileName)
+        {
+            //判斷：不分大小寫
+            if (!fileName.EndsWith(".log", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ LearningUT/LogAnalyzerTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace LearningUT
+{
+    class LogAnalyzerTest
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+            //bool result = analyzer.IsValidLogFileName("filewithbadextension.log");
+            Assert.False(result);   //預期：False。當發生非預期，就會Error，表示程式寫錯
+        }
+
+        [Test]
+        public void IsValidLogFileName_goodExtensionUppercase_ReturnsTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithgoodextension.LOG");
+            Assert.True(result);   //預期：True。當發生非預期，就會Error，表示程式寫錯
+        }
+
+
+    }
+}
