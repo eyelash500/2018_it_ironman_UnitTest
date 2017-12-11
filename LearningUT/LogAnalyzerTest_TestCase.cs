1e7be7c2cdc839f3db468c54255c290b242b05ed
eyelash500 <eyelash94500@gmail.com>
2017-12-11 23:38:38 +0800

    Day 7 test Case

---
+++ LearningUT/LogAnalyzerTest_TestCase.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LearningUT
+{
+    class LogAnalyzerTest_TestCase
+    {
+        [TestCase("WoW.txt")]   //That will be error.
+        [TestCase("SUCCESS.LOG")]
+        [TestCase("TEST.log")]
+        public void IsValidLogFileName_ValidExtensions_ReturnsTrue(string file)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(file);
+            Assert.True(result);   //預期：True。當發生非預期，就會Error，表示程式寫錯
+        }
+
+        
+    }
+}
