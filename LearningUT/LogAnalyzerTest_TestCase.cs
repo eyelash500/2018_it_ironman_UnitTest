56850535e2d83d06daa99797da4e5d2a7c2948ea
eyelash500 <eyelash94500@gmail.com>
2017-12-12 00:09:30 +0800

    Day7: update the assert funciton.

--- LearningUT/LogAnalyzerTest_TestCase.cs
+++ LearningUT/LogAnalyzerTest_TestCase.cs
@@ -4,16 +4,15 @@ namespace LearningUT
 {
     class LogAnalyzerTest_TestCase
     {
-        [TestCase("WoW.txt")]   //That will be error.
-        [TestCase("SUCCESS.LOG")]
-        [TestCase("TEST.log")]
-        public void IsValidLogFileName_ValidExtensions_ReturnsTrue(string file)
+        [TestCase("WoW.txt", false)]   //That will be error.
+        [TestCase("SUCCESS.LOG", true)]
+        [TestCase("TEST.log", true)]
+        public void IsValidLogFileName_ValidExtensions_ReturnsTrue(string file, bool expected)
         {
             LogAnalyzer analyzer = new LogAnalyzer();
             bool result = analyzer.IsValidLogFileName(file);
-            Assert.True(result);   //預期：True。當發生非預期，就會Error，表示程式寫錯
+            Assert.AreEqual(expected, result);   //預期：True。當發生非預期，就會Error，表示程式寫錯
         }
-
         
     }
 }
