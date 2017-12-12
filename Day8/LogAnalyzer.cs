c29ee6be7e105762d555c206b8e1903bb1a97664
eyelash500 <eyelash94500@gmail.com>
2017-12-13 00:39:50 +0800

    Day8: System status.

---
+++ Day8/LogAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day8
+{
+    class LogAnalyzer
+    {
+        public static void Main(){ }
+
+        public bool WasLastFileNameValid { get; set; }
+        public bool IsValidLogFileName(string fileName)
+        {
+            WasLastFileNameValid = false;   //Modify the system's status.
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("請提供檔名 please provide the filename");
+            }
+
+            if(!fileName.EndsWith(".log", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            //Doing the line means everything is correct.
+            //Therefore the system's status is "true".
+            WasLastFileNameValid = true;        //modify the system's status
+
+            return true;        //means: the function finishes.
+        }
+    }
+}
---
+++ Day8/LogAnalyzerTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Day8
+{
+    class LogAnalyzerTest
+    {
+
+        [TestCase("Test.LOG", true)]
+        [TestCase("Test.log", true)]
+        [TestCase("Test.qoo", false)]
+        [TestCase("", false)]   //It will be exception.
+        //Name: TargetFunctionName_Factor_ExpectResult()
+        public void IsValidLogFileName_WhenCalled_ChangeWasLastFileNameValid(string fileName, bool expectResult)
+        {
+            LogAnalyzer logAlr = MakeAnalyzer();        //Arrange   //var: the target object.
+
+            logAlr.IsValidLogFileName(fileName);        //Act   //func:Do something and change the system's status.
+
+            Assert.AreEqual(expectResult, logAlr.WasLastFileNameValid); //Assert    //check: the status is correct.
+        }
+
+        //func: initalize the class "LogAnalyzer"
+        private LogAnalyzer MakeAnalyzer()
+        {
+            return new LogAnalyzer();
+        }
+    }
+}
