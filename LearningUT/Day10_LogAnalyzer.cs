d5cf0c231d8d9f3b72ce702ac78e38ae3b78a207
eyelash500 <eyelash94500@gmail.com>
2017-12-15 00:49:44 +0800

    Day10: stub object.

---
+++ LearningUT/Day10_LogAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿namespace LearningUT
+{
+    class Day10_LogAnalyzer
+    {
+        private IExtensionManager manager;   
+        public Day10_LogAnalyzer(IExtensionManager mgr)
+        {
+            manager = mgr;
+        }
+        //public class FileExtensionManager : IExtensionManager
+        //{
+        //    public bool IsValid(string fileName)
+        //    {
+        //        return true;
+        //    }
+            
+        //}
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            //IExtensionManager mgr = new FileExtensionManager();
+            return manager.IsValid(fileName);
+            //return mgr.IsValid(fileName);
+        }
+    }
+    //interface: define an adapter.
+    public interface IExtensionManager
+    {
+        bool IsValid(string fileName);
+    }
+
+
+}
---
+++ LearningUT/Day10_LogAnalyzer_Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace LearningUT
+{
+    [TestFixture]
+    public class Day10_LogAnalyzer_Test
+    {
+        [Test]
+        public void IsValidFileName_NameSupportedExtension_ReturnsTrue()
+        {
+            FakeExtensionManager myFakeManager = new FakeExtensionManager();
+            myFakeManager.WillBeValid = true;
+
+            Day10_LogAnalyzer log = new Day10_LogAnalyzer(myFakeManager);
+            bool result = log.IsValidLogFileName("test.log");
+            Assert.True(result);
+        }
+        internal class FakeExtensionManager : IExtensionManager
+        {
+            public bool WillBeValid = false;
+            public bool IsValid(string filename)
+            {
+                return WillBeValid;
+            }
+        }
+    }
+}
