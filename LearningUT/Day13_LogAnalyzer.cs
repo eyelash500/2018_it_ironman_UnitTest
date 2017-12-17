78484a51f57b8113cb1d267af24417122ad612ca
eyelash500 <eyelash94500@gmail.com>
2017-12-18 00:33:11 +0800

    Add the classes and functions.

---
+++ LearningUT/Day13_LogAnalyzer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LearningUT
+{
+    //class: the test target.
+    class Day13_LogAnalyzer
+    {
+        private IWebService service;
+
+        //structure: put the interface data to service.
+        public Day13_LogAnalyzer(IWebService service)
+        {
+            this.service = service;
+        }
+
+        public void Analyze(string fileName)
+        {
+            if (fileName.EndsWith(".log", StringComparison.CurrentCultureIgnoreCase))
+            {
+                service.LogMessage("true");
+            }
+            else
+            {
+                service.LogMessage("false");
+            }
+        }
+
+        [Test]
+        public void LogAnalyze_NameSupportedExtension_CallWebservice()
+        {
+            FakeWebService mokeSample = new FakeWebService();
+            Day13_LogAnalyzer log = new Day13_LogAnalyzer(mokeSample);
+            
+            log.Analyze("test_false.false");
+            //log.Analyze("test_true.log");
+
+            StringAssert.Contains("It is log file:", mokeSample.LastMessage);
+        }
+
+    }
+
+    #region Mock
+    //interface: Web service's message.
+    public interface IWebService
+    {
+        void LogMessage(string message);
+    }
+
+    //A fake class.
+    public class FakeWebService : IWebService
+    {
+        public string LastMessage;
+        public void LogMessage(string message)
+        {
+            LastMessage = message;
+        }
+    }
+    #endregion
+}
